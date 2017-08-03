0b9b27e4dc6bf4138bdeb92b554a5d1fd3228aa7
Razvan Neacsu <razvan.neacsu@topor.io>
2017-08-03 12:48:40 +0100

    Add project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace GVision
+{
+	class Program
+	{
+
+		public static string directoryPath;
+		public static string[] pictures;
+
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Introdu cale poze:");
+	
+			directoryPath = Console.ReadLine();
+
+			pictures = Directory.GetFiles(directoryPath);
+
+		}
+	}
+}
